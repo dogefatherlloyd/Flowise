3189f952d3438f5b1bcd09933b60033fc857ec16
Lloyd <131279003+dogefatherlloyd@users.noreply.github.com>
2023-05-31 13:08:59 -0400

    Update index.js

--- packages/ui/src/views/chatflows/index.js
+++ packages/ui/src/views/chatflows/index.js
@@ -83,13 +83,19 @@ const Chatflows = () => {
                 const images = {}
                 for (let i = 0; i < chatflows.length; i += 1) {
                     const flowDataStr = chatflows[i].flowData
-                    const flowData = JSON.parse(flowDataStr)
-                    const nodes = flowData.nodes || []
-                    images[chatflows[i].id] = []
-                    for (let j = 0; j < nodes.length; j += 1) {
-                        const imageSrc = `${baseURL}/api/v1/node-icon/${nodes[j].data.name}`
-                        if (!images[chatflows[i].id].includes(imageSrc)) {
-                            images[chatflows[i].id].push(imageSrc)
+                    if (typeof flowDataStr === 'string') {
+                        try {
+                            const flowData = JSON.parse(flowDataStr)
+                            const nodes = flowData.nodes || []
+                            images[chatflows[i].id] = []
+                            for (let j = 0; j < nodes.length; j += 1) {
+                                const imageSrc = `${baseURL}/api/v1/node-icon/${nodes[j].data.name}`
+                                if (!images[chatflows[i].id].includes(imageSrc)) {
+                                    images[chatflows[i].id].push(imageSrc)
+                                }
+                            }
+                        } catch (e) {
+                            console.error(`Error parsing flowDataStr: ${e}`)
                         }
                     }
                 }
@@ -114,15 +120,13 @@ const Chatflows = () => {
                 </Grid>
             </Stack>
             <Grid container spacing={gridSpacing}>
-                {!isLoading &&
-                    getAllChatflowsApi.data &&
-                    getAllChatflowsApi.data.map((data, index) => (
-                        <Grid key={index} item lg={3} md={4} sm={6} xs={12}>
-                            <ItemCard onClick={() => goToCanvas(data)} data={data} images={images[data.id]} />
-                        </Grid>
-                    ))}
+                {!isLoading && Array.isArray(getAllChatflowsApi.data) && getAllChatflowsApi.data.map((data, index) => (
+                    <Grid key={index} item lg={3} md={4} sm={6} xs={12}>
+                        <ItemCard onClick={() => goToCanvas(data)} data={data} images={images[data.id]} />
+                    </Grid>
+                ))}
             </Grid>
-            {!isLoading && (!getAllChatflowsApi.data || getAllChatflowsApi.data.length === 0) && (
+            {!isLoading && (!getAllChatflowsApi.data || !Array.isArray(getAllChatflowsApi.data) || getAllChatflowsApi.data.length === 0) && (
                 <Stack sx={{ alignItems: 'center', justifyContent: 'center' }} flexDirection='column'>
                     <Box sx={{ p: 2, height: 'auto' }}>
                         <img style={{ objectFit: 'cover', height: '30vh', width: 'auto' }} src={WorkflowEmptySVG} alt='WorkflowEmptySVG' />
